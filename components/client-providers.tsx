ff907a43ac121ad2a0d35bf2d1129adb91c40854
Adebesin Cell <adebesintolulope80@gmail.com>
2024-11-27 13:22:49 +0100

    chore: bump iq login to v6.1.0

--- components/client-providers.tsx
+++ components/client-providers.tsx
@@ -2,16 +2,33 @@
 
 import { IqLoginProvider } from '@everipedia/iq-login/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { polygon } from 'viem/chains';
 
 const queryClient = new QueryClient();
+
+const NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID =
+  process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID;
+const NEXT_PUBLIC_WEB3_AUTH_CLIENT_ID =
+  process.env.NEXT_PUBLIC_WEB3_AUTH_CLIENT_ID;
 
 export default function ClientProviders({
   children
 }: {
   children: React.ReactNode;
 }) {
+  if (!NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID) {
+    throw new Error('Missing NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID');
+  }
+  if (!NEXT_PUBLIC_WEB3_AUTH_CLIENT_ID) {
+    throw new Error('Missing NEXT_PUBLIC_WEB3_AUTH_CLIENT_ID');
+  }
+
   return (
-    <IqLoginProvider>
+    <IqLoginProvider
+      chain={polygon}
+      walletConnectProjectId={NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID}
+      web3AuthProjectId={NEXT_PUBLIC_WEB3_AUTH_CLIENT_ID}
+    >
       <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
     </IqLoginProvider>
   );
